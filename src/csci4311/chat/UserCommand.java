5a4ace1f214493bb5cc27ff094c8fec34490827e
nazar-bukhari <ratul.pentagon@gmail.com>
2015-11-08 14:36:37 +0600

    all command structure set

--- src/csci4311/chat/UserCommand.java
+++ src/csci4311/chat/UserCommand.java
@@ -1,15 +1,14 @@
 package csci4311.chat;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * Created by nazar on 11/8/15.
  */
 public class UserCommand {
 
-    private final String startingWord = "msgp";
+    private final String firstKey = "msgp";
+
 //    private String secondKey = null;
 
     public UserCommand() {
@@ -21,9 +20,8 @@ public class UserCommand {
 
             String[] inputSet = userInput.split(" ");
 
-            if(inputSet[0].equals(startingWord)){
-                System.out.println("Valid ");
-                verifySecondKey(inputSet[1]);
+            if(inputSet[0].equals(firstKey)){
+                verifySecondKey(inputSet,inputSet[1]);
             }
 
         } catch (IOException e) {
@@ -31,11 +29,39 @@ public class UserCommand {
         }
     }
 
-    public void verifySecondKey(String secondKey){
+    public void verifySecondKey(String[] inputSet,String secondKey) throws IOException {
+
+        String user;
+        String group;
+        String fileName ;
 
         switch(secondKey){
+
             case "join" :
                 System.out.println("join command");
+                user = inputSet[2];
+                group = inputSet[3];
+                File f = new File(group+".txt");
+                fileName = f.getName();
+                if(f.exists()){
+
+                    FileWriter ostream = new FileWriter(fileName,true);
+                    BufferedWriter out = new BufferedWriter(ostream);
+                    out.write("\n");
+                    out.write(user);
+                    out.flush();
+                    out.close();
+                }
+                else{
+                    System.out.println("No "+f.getName());
+                    try {
+                        FileWriter writer = new FileWriter(fileName);
+                        writer.write(user);
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
                 break;
             case "leave" :
                 System.out.println("leave command");
@@ -53,7 +79,7 @@ public class UserCommand {
                 System.out.println("history command");
                 break;
             default:
-                System.out.println("Comamnd not allowed");
+                System.out.println("Comamnd not found");
 
         }
     }
